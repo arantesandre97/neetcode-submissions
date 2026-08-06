43d616538f0a8f89e437fbaba0d9e6c67f0b289c
Andre Arantes <28788732+arantesandre97@users.noreply.github.com>
2026-08-05 22:38:46 -0300

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.java
@@ -0,0 +1,35 @@
+class Solution {
+    //As it need to have no duplicates
+    //the best approach would be sort the array
+    //in that way we can skip numbers already picked
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triplets = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 2; i++) {
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while(j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum == 0) {
+                    triplets.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while(j < k && nums[j] == nums[j+1]) j++;
+                    while(j > k && nums[k] == nums[k-1]) k--;
+                    j++;
+                    k--;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        return triplets;
+    }
+}
